8a599a1b7693530e472861eb61f82adc00f30707
Neko-pararo <715046899>
2021-03-05 11:37:49 +0800

    第二周

---
+++ Eratosthenes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Eratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("用埃氏筛法筛出的，2-100的素数有：");
+            int n = 100;
+            bool[] array = new bool[n + 1];
+            for (int i = 2; i < n; i++)
+            {
+                array[i] = true;
+            }
+            for (int i = 2; i < n; i++)
+            {
+                if (array[i])
+                {
+                    for (int j =2; j*i < n; j++)
+                    {
+                        array[j*i] = false;
+                    }
+                }
+            }
+            for(int i = 2; i < n; i++)
+            {
+                if (array[i])
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ ZhiYinZi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ZhiYinZi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入想要求质因子的整数");
+            int n= int.Parse(Console.ReadLine());
+            Console.WriteLine("所得所有质因子如下：");
+            Zyz zyz = new Zyz();
+            zyz.GetZyz(n);
+
+        }
+    }
+}
+    class Zyz
+{
+    public void GetZyz(int x)
+    {
+        for(int i = 2; i<=x; i++)
+        {
+            if(x%i == 0)
+            {
+                x = x / i;
+                Console.WriteLine(x + "");
+            }
+        }
+    }
+}    
