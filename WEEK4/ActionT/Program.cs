b9eee985e99beb2709645fe2d1f42160da2c0f5e
Neko-pararo <715046899>
2021-03-19 10:19:43 +0800

    .

---
+++ WEEK4/ActionT/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+/*为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。
+  通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）*/
+namespace ActionT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+            {
+                GenericList<int> list = new GenericList<int>();
+                for (int i = 0; i < 10; i++)
+                {
+                    list.Add(i);
+                }
+                int max = list.Head.Data;
+                int min = list.Head.Data;
+                int sum = 0;
+                list.ForEach(n => Console.WriteLine(n));
+                list.ForEach(n => sum += n);
+                list.ForEach(n => { max = max > n ? max : n; });
+                list.ForEach(n => { min = min < n ? min : n; });
+
+                Console.WriteLine("max：" + max);
+                Console.WriteLine("min：" + min);
+                Console.WriteLine("sum：" + sum);
+
+
+            }
+        
+    }
+}
+// 根据示例代码写出泛型链表类
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+}
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public GenericList()
+    {
+        tail = head = null;
+    }
+
+    public Node<T> Head
+    {
+        get => head;
+    }
+
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+            if(tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        
+    }
+    // 实现foreach方法
+    public void ForEach(Action<T> action)
+    {
+         for (Node<T> m = head; m.Next != null; m = m.Next)
+            {
+                action(m.Data);
+            }
+        }
+
+    }
+
---
+++ WEEK4/TickAndAlarm/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+/* 使用事件机制，模拟实现一个闹钟功能。
+ * 闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+ * 在闹钟走时时或者响铃时，在控制台显示提示信息*/
+namespace TickAndAlarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            DateTime time = new DateTime();
+            time = DateTime.Now.AddSeconds(10);
+            clock.SetAlarm(time);
+            clock.Start();
+        }
+    }
+}
+//实现功能的clock类
+class Clock
+{
+    DateTime alarmTime = new DateTime();
+    //事件委托定义
+    public delegate void AlarmHandler(object sender, DateTime args);
+    public delegate void TickHandler(object sender, DateTime args);
+    //事件创建
+    public event AlarmHandler AlarmOn;
+    public event TickHandler TickOn;
+    //在构造方法中进行事件订阅
+    public Clock() {
+        AlarmOn += Alarm;
+        TickOn += Tick;
+    }
+    //编写Alarm和Tick的方法
+    public void Alarm(object sender,DateTime time)
+    {
+        Console.WriteLine("闹钟报时：" + time);
+    }
+    public void Tick(object sender,DateTime time)
+    {
+        Console.WriteLine("北京时间：" + time);
+    }
+
+    //补充测试中用到的功能
+    public void SetAlarm(DateTime time)
+    {
+        Console.WriteLine(time);
+        alarmTime = time;
+    }
+    public void Start()
+    {
+        while (true)
+        {
+            DateTime now = DateTime.Now;
+            TickOn(this, now);
+            if (now.ToString() == alarmTime.ToString())
+            {
+            AlarmOn(this, alarmTime);
+            }
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
